f0350c875d25c4d4f293df40db0535f7aa143307
studentidle <jedale.sabio@student.ateneo.edu>
2026-02-23 15:57:32 +0800

    added urls for signup,login and dashboard

--- serbisyongScholars/scholarapp/urls.py
+++ serbisyongScholars/scholarapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/signup/', views.signup, name='signup'),
-    path('auth/login/', views.login, name='login'),
-    path('scholar/dashboard/', views.get_scholar_dashboard, name='dashboard'),
+    path('api/signup/', views.signup, name='signup'),
+    path('api/login/', views.login, name='login'),
+    path('api/verify/', views.verify_email, name='verify_email'),
+    path('api/dashboard/', views.get_scholar_dashboard, name='dashboard'),
 ]
