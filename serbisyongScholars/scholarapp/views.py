cb7dd0fa1cfc9a513ae6e9a628c6e0097e9e4a50
studentidle <jedale.sabio@student.ateneo.edu>
2026-02-23 15:19:09 +0800

    added a register function
    - handles basic email, username, password, first_name, last_name
    - sends confirmation link to the owner if registration is successful
    - email must be a valid ateneo email

--- serbisyongScholars/scholarapp/views.py
+++ serbisyongScholars/scholarapp/views.py
@@ -11,4 +11,34 @@ from django.conf import settings
 
 from django.shortcuts import render
 
-# Create your views here.
+# Register a new user by default it is a scholar
+# -- SignUp View -- #
+@api_view(['POST'])
+def register(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+    
+    if not email.endswith('@student.ateneo.edu') and not email.endswith('@ateneo.edu'):
+        return Response({'error': 'Must use a valid Ateneo email.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        user = User.objects.create_user(
+            username=first_name + last_name,
+            email=email, 
+            password=password, 
+            first_name=first_name, 
+            last_name=last_name,
+            is_active=False
+        )
+        scholar_group, created = Group.objects.get_or_create(name='Scholar')
+        user.groups.add(scholar_group)
+
+        send_confirmation_email(user)
+        return Response({'message': 'Registration successful. Please check your email to confirm your account.'}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    
